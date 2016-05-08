5f7e662290da42f51be26de4431247a851d74908
Shawn Anderson <ravensorb@outlook.com>
2016-05-08 18:09:22 -0400

    Initial version of build based on psake

--- psake.build.ps1
+++ psake.build.ps1
@@ -12,15 +12,16 @@ Param(
     [string]$configuration = "Release",
 	
 	[Parameter()]
-	[bool]$versionUpdate = $false
+	[bool]$updateVersion = $false,
 		
 	[Parameter()]
 	[bool]$updateNuspecFile = $true,
+	
 	[Parameter()]
-	[bool]$updateNuspecVersion = $true,
+	[bool]$updateNuspecVersion = $true
 )
 
-$build_file = 'xlabs-psake-default.ps1'
+$build_file = 'psake.default.ps1'
 
 # Properties for the psake build script
 $properties = @{
@@ -32,39 +33,56 @@ $properties = @{
     # This will be read from the command line args
     "version"       = $version;
 
+	# Is the Nuget package a pre-release version?
+	"preRelease" = $preRelease;
+
     # Path to the solution file
     "solution"      = 'XLabs.sln';
 
     # Folder containing source code
-    "source_folder" = 'source';
+    "source_folder" = '.\source';
+	
+	# Folder container unit tests
+	"test_folder" = '.\tests';
 
     # Folder to output deployable packages to. This folder should be ignored
     # from any source control, as we dont commit build artifacts to source
     # control
-    "deploy_folder" = 'deploy';
+    "deploy_folder" = '.\artifacts';
 
 	# Folder that contains nuget files (nuget.exe, nuget.config)
-	"nuget_folder" = ".nuspec";
+	"nuget_folder" = '.\.nuget';
 
 	# Folder that contains nuspec files
-	"nuspec_folder" = ".nuspec\assets";
+	"nuspec_folder" = '.\.nuget\artifacts';
+	
+	# Folder that contains nuspec files
+	"nuproj_folder" = '.\.nuget\source';
 	
 	# List of projects to use when building NuGet Packages (Note: Not used for XLabs)
     "projects" = @(
 	);
 	
-	# Is the Nuget package a pre-release version?
-	"preRelease" = $preRelease;
-
+	# Unit Test Framework (nunit, xunit)
+	"unittest_framework" = "nunit";
+	
 	# Update the version numbers automatically
-	"versionUpdate" = $versionUpdate
+	"updateVersion" = $updateVersion;
 	
 	"updateNuspecVersion" = $updateNuspecVersion;
 	
 	"updateNuspecFile" = $updateNuspecFile;
+	
+	# The name or ip address of the Mac that is running the Xamarin build agent
+	"macAgentServerAddress" = "10.0.1.139"
+	
+	# The user name to use to authentice for the Xamarin build agent
+	"macAgentUser" = "Shawn Anderson"
 }
+
+#if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser }
 
 #import-module .\packages\psake.4.4.2\tools\psake.psm1
 import-module C:\ProgramData\chocolatey\lib\psake\tools\psake.psm1
 
-invoke-psake $build_file $task_list -Properties $properties
+invoke-psake $build_file $task_list -Properties $properties -Framework "4.6"
--- psake.default.ps1
+++ psake.default.ps1
@@ -8,40 +8,66 @@ properties {
     $deploy_folder = "deploy"
 	$nuget_folder = ".nuget"
 	$nuspec_folder = $null
+	$nuproj_folder = $null
     $projects = $null
-	$versionUpdate = $true
+	$updateVersion = $true
 	$updateNuspecVersion = $true
 	$updateNuspecFile = $true
 	$unittest_framework = "nunit"
+	$macAgentServerAddress = $null
+	$macAgentUser = $null
 }
 
-Task Default -Depends Build
+Task Default -Depends DisplayParams,Build
+
+Task DisplayParams {
+    Write-Host "`tconfiguration : $configuration"
+	Write-Host "`tversion : $version"
+	Write-Host "`tpreRelease : $preRelease"
+    Write-Host "`tsolution : $solution"
+    Write-Host "`tsource_folder : $source_folder"
+	Write-Host "`ttest_folder : $test_folder"
+    Write-Host "`tdeploy_folder : $deploy_folder"
+	Write-Host "`tnuget_folder : $nuget_folder"
+	Write-Host "`tnuspec_folder : $nuspec_folder"
+	Write-Host "`tnuproj_folder : $nuproj_folder"
+	Write-Host "`tupdateVersion : $updateVersion"
+	Write-Host "`tupdateNuspecVersion : $updateNuspecVersion"
+	Write-Host "`tupdateNuspecFile : $updateNuspecFile"
+	Write-Host "`tunittest_framework : $unittest_framework"
+	Write-Host "`tmacAgentServerAddress : $macAgentServerAddress"
+	Write-Host "`tmacAgentUser : $macAgentUser"
+    Write-Host "`tprojects : $projects"
+}
 
 Task RestorePackages {
-	Exec { $nuget_folder\nuget.exe restore -PackagesDirectory $source_folder\packages }
+	New-Item -ItemType Directory  "$source_folder\packages" -ErrorAction SilentlyContinue
+	$pathToPackages = Resolve-Path "$source_folder\packages"
+	$nugetConfig = Resolve-Path "$nuget_folder\nuget.config"
+	Exec { & "$nuget_folder\nuget.exe" restore "$source_folder\$solution" -MSBuildVersion 14 -PackagesDirectory $pathToPackages -ConfigFile $nugetConfig  }
+	Exec { & "$nuget_folder\nuget.exe" install NuProj -OutputDirectory $pathToPackages -ConfigFile $nugetConfig -Prerelease }
 }
 
 Task RestoreDependencies {
-	switch ($unittest_framework)
-	{
-		"nunit" { choco install -y nunit }
-		"xunit" { choco install -y xunit }
-	}
+#	switch ($unittest_framework)
+#	{
+#		"nunit" { choco install -y nunit }
+#	}
 }
 
-Task Publish -Depends Package {
+Task Publish -Depends DisplayParams,Package {
 	$version = getVersionBase
 	
 	$projects | % {
-		Get-ChildItem | Where-Object -FilterScript {
+		Get-ChildItem -Path $deploy_folder | Where-Object -FilterScript {
 			($_.Name.Contains("$project.$version")) -and !($_.Name.Contains(".symbols")) -and ($_.Extension -eq '.nupkg')    
 		} | % {
-			exec { $nuget_folder\nuget.exe push $_.Fullname }
+			exec { & "$nuget_folder\nuget.exe" "push" "$($_.Fullname)" }
 		}
 	}
 }
 
-Task Package -Depends RestoreDependencies { #-Depends Test {
+Task Package -Depends DisplayParams,RestoreDependencies { #-Depends Test {
 	$version = getVersionBase
 	
 	if ($preRelease) {
@@ -67,7 +93,7 @@ Task Package -Depends RestoreDependencies { #-Depends Test {
 			if (-Not ($updateNuspecFile)) { $nuspecFile = $nuSpecFilePathTmp; }
 		}
 				
-		exec { $nuget_folder\nuget.exe pack $nuspecFile -OutputDirectory $deploy_folder -Version $version -Symbols -Prop Configuration=$configuration }
+		exec { & "$nuget_folder\nuget.exe" "pack" "$nuspecFile" "-OutputDirectory $deploy_folder" "-Version $version" "-Symbols" "-Prop Configuration=$configuration" }
 
 		if (-Not ($updateNuspecFile)) { Remove-Item $nuSpecFilePathTmp -ErrorAction SilentlyContinue }
 	}
@@ -87,25 +113,25 @@ Task Test -Depends Build {
 }
 
 Task Build -Depends Clean,Set-Versions,RestorePackages,RestoreDependencies {
-	Exec { msbuild "$solution" /t:Build /p:Configuration=$configuration } 
+	Exec { msbuild "$source_folder\$solution" /t:Build /p:Configuration=$configuration /consoleloggerparameters:"ErrorsOnly;WarningsOnly" /p:ServerAddress=$macAgentServerAddress /p:ServerUser=$macAgentUser } 
 }
 
-Task Clean {
-	Exec { msbuild "$solution" /t:Clean /p:Configuration=$configuration } 
+Task Clean -Depends DisplayParams {
+	Exec { msbuild "$source_folder\$solution" /t:Clean /p:Configuration=$configuration /consoleloggerparameters:"ErrorsOnly" /p:ServerAddress=$macAgentServerAddress /p:ServerUser=$macAgentUser } 
 	
-	gci -Path $source_folder,$test_folder,$nuspec_folder -Recurse -include 'bin','obj' | % {
+	gci -Path $source_folder,$test_folder,$deploy_folder,$nuspec_folder,$nuproj_folder -Recurse -include 'bin','obj' -ErrorAction SilentlyContinue | % {
 		remove-item $_ -recurse -force
 		write-host deleted $_
 	}
 }
 
 Task Set-Versions {
-	if (-Not $versionUpdate) return
+	if (-Not $updateVersion) { return }
 	
 	$version = getVersionBase
 
-	Get-ChildItem -Recurse -Force | Where-Object { $_.Name -eq "AssemblyInfo.cs" } | ForEach-Object {
-		(Get-Content $_.FullName) | ForEach-Object {
+	Get-ChildItem -Recurse -Force | ? { $_.Name -eq "AssemblyInfo.cs" -or $_.Name -eq "AssemblyInfo.Shared.cs" } | % {
+		(Get-Content $_.FullName) | % {
 			($_ -replace 'AssemblyVersion\(.*\)', ('AssemblyVersion("' + $version + '")')) -replace 'AssemblyFileVersion\(.*\)', ('AssemblyFileVersion("' + $version + '")')
 		} | Set-Content $_.FullName -Encoding UTF8
 	}    
@@ -116,19 +142,19 @@ Task Set-Versions {
 function getVersionBase {
 	if ($version -eq $null) {
 		$versionInfo = (Get-Content "version.json") -join "`n" | ConvertFrom-Json
-		"$($versionInfo.major).$($versionInfo.minor).$($versionInfo.patch)";    
+		"$($versionInfo.major).$($versionInfo.minor).$($versionInfo.build)";    
 	} else {
 		$version
 	}
 }
 
-function ChangeNuSpecVersion([string] $baseNamespace, [string] $nuSpecFilePath, [string] $version="0.0.0.0", [string] $nuSpecFilePathTmp = $null, [bool] $dependencyVersionUpdate = $false, [string] $dependencyVersion = $null)
+function ChangeNuSpecVersion([string] $baseNamespace, [string] $nuSpecFilePath, [string] $version="0.0.0.0", [string] $nuSpecFilePathTmp = $null, [bool] $dependencyupdateVersion = $false, [string] $dependencyVersion = $null)
 {
 	Write-Host "Dynamically setting NuSpec version: $Version" -ForegroundColor Yellow
 	Write-Host "`tNuspec File: $nuSpecFilePath"
 	Write-Host "`tTemp Nuspec File: $nuSpecFilePathTmp"
 	Write-Host "`tBase Namespace: $baseNamespace"
-	Write-Host "`tUpdate Dependency Version: $dependencyVersionUpdate"
+	Write-Host "`tUpdate Dependency Version: $dependencyupdateVersion"
 	
 	# Get full path or save operation fails when launched in standalone powershell
 	$nuSpecFile = Get-Item $nuSpecFilePath | Select-Object -First 1
@@ -147,7 +173,7 @@ function ChangeNuSpecVersion([string] $baseNamespace, [string] $nuSpecFilePath, 
 	}
 	
 	# Update the XML document dependencies with the new version
-	if ($dependencyVersionUpdate)
+	if ($dependencyupdateVersion)
 	{
 		Write-Host "Updating Dependencies" -ForegroundColor Yellow
 		$dependencies = $xDoc.Descendants( $xmlns + "dependency" )
