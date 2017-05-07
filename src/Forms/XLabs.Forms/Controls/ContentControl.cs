61dc2c9ff908f24e13522efb6bd03deac60f8b31
Shimmy <weitzhandler@users.noreply.github.com>
2017-05-07 04:24:29 +0300

    Update ContentControl.cs

--- src/Forms/XLabs.Forms/Controls/ContentControl.cs
+++ src/Forms/XLabs.Forms/Controls/ContentControl.cs
@@ -1,69 +1,40 @@
-﻿// ***********************************************************************
-// Assembly         : XLabs.Forms
-// Author           : XLabs Team
-// Created          : 12-27-2015
-// 
-// Last Modified By : XLabs Team
-// Last Modified On : 01-04-2016
-// ***********************************************************************
-// <copyright file="ContentControl.cs" company="XLabs Team">
-//     Copyright (c) XLabs Team. All rights reserved.
-// </copyright>
-// <summary>
-//       This project is licensed under the Apache 2.0 license
-//       https://github.com/XLabs/Xamarin-Forms-Labs/blob/master/LICENSE
-//       
-//       XLabs is a open source project that aims to provide a powerfull and cross 
-//       platform set of controls tailored to work with Xamarin Forms.
-// </summary>
-// ***********************************************************************
-// 
-
 using Xamarin.Forms;
 
 namespace XLabs.Forms.Controls
 {
+  /// <summary>
+  /// A view that renders its content based on a data template. Typical usage is to either set an explicit 
+  /// <see cref="BindableObject.BindingContext"/> on this element or use an inhereted one, then set a display.
+  /// </summary>
+  [ContentProperty(nameof(ContentTemplate))]
+  public class DataContentView : ContentView
+  {
     /// <summary>
-    /// A view that renders its content based on a data template. Typical usage is to either set an explicit 
-    /// <see cref="BindableObject.BindingContext"/> on this element or use an inhereted one, then set a display.
+    /// The content template property
     /// </summary>
-    public class ContentControl : ContentView
+    public static readonly BindableProperty ContentTemplateProperty = BindableProperty.Create(nameof(ContentTemplate), typeof(DataTemplate), typeof(DataContentView), null);
+    /// <summary>
+    /// A <see cref="DataTemplate"/> used to render the view. This property is bindable.
+    /// </summary>
+    public DataTemplate ContentTemplate
     {
-        /// <summary>
-        /// The content template property
-        /// </summary>
-        public static readonly BindableProperty ContentTemplateProperty = BindableProperty.Create<ContentControl, DataTemplate>(x => x.ContentTemplate, null, propertyChanged: OnContentTemplateChanged);
-
-        private static void OnContentTemplateChanged(BindableObject bindable, object oldvalue, object newvalue)
-        {
-            var cp = (ContentControl)bindable;
-
-            var template = cp.ContentTemplate;
-            if (template != null)
-            {
-                var content = (View)template.CreateContent();
-                cp.Content = content;
-            }
-            else
-            {
-                cp.Content = null;
-            }
-        }
-
-        /// <summary>
-        /// A <see cref="DataTemplate"/> used to render the view. This property is bindable.
-        /// </summary>
-        public DataTemplate ContentTemplate
-        {
-            get
-            {
-                return (DataTemplate)GetValue(ContentTemplateProperty);
-            }
-            set
-            {
-                SetValue(ContentTemplateProperty, value);
-            }
-        }
-
+      get => (DataTemplate)GetValue(ContentTemplateProperty);
+      set => SetValue(ContentTemplateProperty, value);
     }
+
+    protected override void OnBindingContextChanged()
+    {
+      var template = ContentTemplate;
+
+      if (template is DataTemplateSelector dts)
+      {
+        var item = GetValue(BindingContextProperty);
+        template = dts.SelectTemplate(item, this);
+      }
+
+      Content = (View)template?.CreateContent();
+    }
+
+
+  }
 }
